53b3aee2fcf48ec60aaac1a2852e269771c2a9be
Anobium <17933379+Anobium@users.noreply.github.com>
2022-03-01 18:34:38 +0000

    Update all

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Help
+# The Great Cow BASIC Help repository
+
+These are the pages and the toolchain used to generate the Great Cow BASIC Help system.
+
+The source files, in ADOC format, are here: https://github.com/Anobium/Great-Cow-BASIC-Help/tree/master/source
+
+For the online operation Help, see here: http://gcbasic.sourceforge.net/help
+
+For the daily build, see here: http://gcbasic.sourceforge.net/help/output/
---
+++ source/dailybuild.cmd
@@ -0,0 +1,23 @@
+echo off
+cls
+if exist ..\output\chm\gcbasic.chm t del ..\output\chm\gcbasic.chm
+if NOT exist "C:\Program Files\FileZilla FTP Client\psftp.exe"  (
+echo C:\Program Files\FileZilla FTP Client\psftp.exe required
+pause
+goto fin
+)
+
+if NOT exit ..\prog\utils\installNote.txt (
+echo Unpack GCBdoc_TooChain.zip to create the "prog" folders
+pause
+goto fin
+
+)
+
+svn up ..\..\..\help.git
+call all
+:jump1
+if exist ..\output\chm\gcbasic.chm (
+"C:\Program Files\FileZilla FTP Client\psftp.exe"  -v great-cow-basic@web.sourceforge.net -pw %1 -b BatchFile.psftp
+)
+:fin
